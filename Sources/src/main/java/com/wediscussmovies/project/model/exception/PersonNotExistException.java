bdeb8f2fc7e9344747af2a05dd4c5d684469a4bd
Mato-77 <56981531+Mato-77@users.noreply.github.com>
2023-01-04 02:15:30 +0100

    Delete Sources/src/main directory

--- Sources/src/main/java/com/wediscussmovies/project/model/exception/PersonNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.wediscussmovies.project.model.exception;
-
-public class PersonNotExistException extends RuntimeException{
-    public PersonNotExistException(Integer id){
-        super("Личност со број " + id + " не постои!");
-    }
-}
--- Sources/src/main/java/com/wediscussmovies/project/model/exception/ReplyNotExistException.java
+++
@@ -1,9 +0,0 @@
-package com.wediscussmovies.project.model.exception;
-
-import com.wediscussmovies.project.model.primarykeys.ReplyPK;
-
-public class ReplyNotExistException extends RuntimeException{
-    public ReplyNotExistException(ReplyPK id){
-        super("Репликата со број " + id + " не постои!");
-    }
-}
